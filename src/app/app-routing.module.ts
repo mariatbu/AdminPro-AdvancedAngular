670c20c636b6c10e874be8d15750da98056e1a12
mariatbu <maria.turpin.buendia98@gmail.com>
2022-07-10 13:00:32 +0200

    Implementing secondary routes and moving styles to pages component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,15 +5,24 @@ import { RegisterComponent } from './auth/register/register.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { Graph1Component } from './pages/graph1/graph1.component';
 import { NotfoundComponent } from './pages/notfound/notfound.component';
+import { PagesComponent } from './pages/pages.component';
 import { ProgressComponent } from './pages/progress/progress.component';
 
 const routes: Routes = [
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'login', component: LoginComponent },
+  { 
+    path: '',
+    component: PagesComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'progress', component: ProgressComponent },
+      { path: 'graph1', component: Graph1Component },
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+    ]
+  },
+
   { path: 'register', component: RegisterComponent },
-  { path: 'progress', component: ProgressComponent },
-  { path: 'graph1', component: Graph1Component },
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+
   { path: '**', component: NotfoundComponent },
 ];
 
